0806a042bc97c2b704846c5efd190d7908eb5bdc
ocoa <shawn@ocoa.com>
2018-10-07 16:40:37 -0400

    add mongo

---
+++ mongo/init.sh
@@ -0,0 +1,13 @@
+set -e
+
+mongo <<EOF
+var db = connect("mongodb://localhost/$MONGO_INITDB_DATABASE");
+db.createUser({
+  user:  '$MONGO_INITDB_ROOT_USERNAME',
+  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_INITDB_DATABASE'
+  }]
+})
+EOF
