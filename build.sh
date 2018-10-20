c94b139a5c05aeee8d3371d4d02b1891ba7ebcc3
ocoa <shawn@ocoa.com>
2018-10-20 11:24:18 -0400

    ..

--- build.sh
+++ build.sh
@@ -1,13 +1,21 @@
 
-#- ./build.sh php/alpine/php-fpm-7.2 techcto alpine-php-fpm-7.2
-# cd ../php/alpine/php-fpm-7.2
+#- IE: ./build.sh php/alpine/php-fpm-7.2 techcto alpine-php-fpm-7.2
 TAG=$2/$3
+
 docker images $TAG
+
 docker build --tag $TAG $1/.
-mkdir -p input output
-docker save $TAG > input/$3.tar
-docker run -v $(pwd)/input:/input -v $(pwd)/output:/output -v /tmp -i myyk/docker-squash -i input/$3.tar -o output/$3.tar
-cat output/$3.tar | docker load
-ls -alh input/* && ls -alh output/*
+
+#Method 1
+# mkdir -p input output
+# docker save $TAG > input/$3.tar
+# docker run -v $(pwd)/input:/input -v $(pwd)/output:/output -v /tmp -i myyk/docker-squash -i input/$3.tar -o output/$3.tar
+# cat output/$3.tar | docker load
+# ls -alh input/* && ls -alh output/*
+
+#Method 2
+docker save $TAG | docker run  -v /tmp -i myyk/docker-squash -from root -t $TAG:latest | docker load
+
 docker push $TAG
+
 docker images $TAG
