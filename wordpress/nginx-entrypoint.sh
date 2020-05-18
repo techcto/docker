4f81b4f6cd034b228c99880b692e189a96e76902
techcto <smoore@solodev.com>
2020-05-18 14:03:27 -0400

    ..

---
+++ wordpress/nginx-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eo pipefail
+
+# Update App URL
+sed -i -e 's/{{APP_URL}}/${APP_URL}/g' /etc/nginx/conf.d/nginx.conf
+
+if [ -z ${APP_ENV+x} ] && [ "$APP_ENV" != "dev" ]; then
+    #Update Hosts file to resolve local solodev
+    echo "#PHP-FPM" >> /etc/hosts
+    echo "127.0.0.1 ${APP_URL}" >> /etc/hosts
+fi
+
+COMMAND=${COMMAND:="nginx"}
+
+${COMMAND}
