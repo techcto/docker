61ab32b6401cf4b1355580599c29458510fb05e3
techcto <smoore@solodev.com>
2020-05-20 07:45:29 -0400

    ..

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 # Install Wordpress
 if ! $(wp core is-installed); then
-    wp core install --url="${WORDPRESS_WEBSITE_URL}"  --title="Default Website" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --allow-root
+    wp core install --allow-root --path=/var/www/html --url="${WORDPRESS_WEBSITE_URL}"  --title="Default Website" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}"
 fi
 
 exec "$@"
