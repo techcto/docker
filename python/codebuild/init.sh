96fdd160db9c5a898e898f66791cfc9012f6b919
techcto <smoore@solodev.com>
2021-09-24 14:02:23 -0400

    ..

--- python/codebuild/init.sh
+++ python/codebuild/init.sh
@@ -1,3 +1,8 @@
-yum install -y zip python3-pip python3 python3-setuptools openssl
+yum install -y zip python3-pip python3 python3-setuptools openssl unzip docker
 curl -qL -o jq https://stedolan.github.io/jq/download/linux64/jq && chmod +x ./jq
-pip3 install awscli
+pip3 install docker-compose
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+aws --version
