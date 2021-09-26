392fc444c5828cbf9a038525233ea5d684c5c383
techcto <smoore@solodev.com>
2021-09-26 08:37:48 -0400

    ..

---
+++ packer/init.sh
@@ -0,0 +1,10 @@
+apt update
+apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt update
+apt install packer
+packer --version
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
