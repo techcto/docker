a5df554b3351e2c4fd7d391bfe07c54101040575
techcto <smoore@solodev.com>
2024-07-02 08:15:15 -0400

    add jq

--- php/aws/php-fpm-8.1/init.sh
+++ php/aws/php-fpm-8.1/init.sh
@@ -2,7 +2,7 @@
 yum -y remove php* httpd*
 
 #Install Required Devtools
-yum -y install gcc-c++ gcc pcre-devel make zip unzip wget curl cmake git yum-utils sudo sendmail tar scl-utils
+yum -y install gcc-c++ gcc pcre-devel make zip unzip wget curl cmake git yum-utils sudo sendmail tar scl-utils jq
 
 #Install Required Repos
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
