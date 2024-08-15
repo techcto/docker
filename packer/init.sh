a9beb05a608e6361132f3b204410b97a44a63d25
techcto <smoore@solodev.com>
2024-08-15 06:22:06 -0400

    ..

--- packer/init.sh
+++ packer/init.sh
@@ -1,8 +1,12 @@
-curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update && apt-get install packer
 
-packer --version
+
+
+apt install -y unzip
+wget https://releases.hashicorp.com/packer/1.11.0/packer_1.11.0_linux_amd64.zip
+unzip packer_1.11.0_linux_amd64.zip
+chmod +x packer
+mv packer /usr/bin/packer
+packer version
 
 # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 # unzip awscliv2.zip
